e95d5034a07dea622f0bb8fa9232eb39dd2c3758
abdullah <ranahaani@gmai.com>
2021-02-25 14:21:15 +0500

    update readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='gnews',
-    version='0.0.3',
+    version='0.0.4',
     author="Muhammad Abdullah",
     author_email="ranahaani@gmail.com",
     description='Search Google News RSS Feed and returns a usable JSON response',
