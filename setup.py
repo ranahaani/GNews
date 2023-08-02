b6a4f671077d50905c6d8ee17f60d6a9727adb13
Muhammad Abdullah <ranahaani@gmail.com>
2023-08-02 19:37:51 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='gnews',
-    version='0.2.7',
+    version='0.2.8',
     author="Muhammad Abdullah",
     author_email="ranahaani@gmail.com",
     description='Provide an API to search for articles on Google News and returns a usable JSON response.',
