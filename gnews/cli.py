d42a6fca7e68c59cc13073f39cc9c172508f8670
Hayden Marshall <marshall.hayden65@gmail.com>
2025-10-22 10:43:28 -0500

    added rest of flags to each feature

--- gnews/cli.py
+++ gnews/cli.py
@@ -1,27 +1,33 @@
 import typer
+import gnews
 from typing_extensions import Annotated
-from gnews.utils.constants import AVAILABLE_COUNTRIES, AVAILABLE_LANGUAGES, SECTIONS, TOPICS, BASE_URL, USER_AGENT
 
 app = typer.Typer()
 
 @app.command()
 def search(
     keyword: Annotated[str, typer.Argument()], 
-    limit: Annotated[int, typer.Option(help="Limit the number of articles received")],
+    limit: Annotated[int, typer.Option(help="Limit the number of articles received")] = 100,
     format: Annotated[str, typer.Option(help="Determine the format of output (json, csv, table)")] = "json"
     ):
+    google_news = gnews.GNews(max_results=limit)
+    articles = google_news.get_news(keyword)
+    print(articles)
     pass
 
 @app.command()
 def trending(
-    country: Annotated[str, typer.Option(help="The country from which to get trending news from")] = "US"
+    country: Annotated[str, typer.Option(help="The country from which to get trending news from")] = "US",
+    limit: Annotated[int, typer.Option(help="Limit the number of articles received")] = 100,
+    format: Annotated[str, typer.Option(help="Determine the format of output (json, csv, table)")] = "json"
     ):
     pass
 
 @app.command()
 def topic(
     topic: Annotated[str, typer.Argument(help="What topic to get news related to")],
-    limit: Annotated[int, typer.Option(help="Limit the number of articles received")],
+    limit: Annotated[int, typer.Option(help="Limit the number of articles received")] = 100,
+    format: Annotated[str, typer.Option(help="Determine the format of output (json, csv, table)")] = "json"
     ):
     # check if topic is in 
     pass
@@ -29,6 +35,7 @@ def topic(
 @app.command()
 def location(
     location: Annotated[str, typer.Argument(help="Where news received will be related to.")],
+    limit: Annotated[int, typer.Option(help="Limit the number of articles received")] = 100,
     format: Annotated[str, typer.Option(help="Determine the format of output (json, csv, table)")] = "json"
     ):
     pass
